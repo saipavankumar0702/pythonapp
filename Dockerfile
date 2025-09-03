cd472759417803d540cb8ec56c9d95d28199cc9a
saipavankumar0702 <saipavan.0702@gmail.com>
2025-09-03 22:06:23 +0530

    v10

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM python:3.11
 WORKDIR /app
 COPY app.py .
 RUN pip install flask
+EXPOSE 5000
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -30,3 +30,6 @@ def hello():
       </body>
     </html>
     '''
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
